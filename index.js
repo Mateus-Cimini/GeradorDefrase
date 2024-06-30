ebd298b7d4f873e48ae41bf88e0e62b5efbd9066
Mateus Cimini Araujo <mateusmateus122333@gmail.com>
2024-06-29 23:00:33 -0300

    add arquivos

---
+++ index.js
@@ -0,0 +1,19 @@
+function mensagem(event){
+    event.preventDefault()
+
+    let name = document.getElementById("inputName").value;
+    let years = document.getElementById("inputYears").value;
+    let height = document.getElementById("inputHeight").value;
+    let contentResult = document.getElementById("result");
+    let mensagem = document.getElementById("message");
+
+    alert(name)
+
+    mensagem.innerHTML = "meu nome e " + name + " tenho " + years + " anos e " + height + " de altura";
+
+    contentResult.classList.remove("hide")
+
+    
+
+
+}
